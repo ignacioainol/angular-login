8e5781415a925b1f3725f525c6e4fe6b2b121e0f
Ignacio Ainol <iggnaxios@gmail.com>
2023-07-06 09:55:56 -0400

    dry

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { AuthService } from '../../services/auth.service';
 import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -10,17 +11,18 @@ import Swal from 'sweetalert2';
 export class LoginPageComponent {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
+  private router = inject(Router)
 
   public myForm: FormGroup = this.fb.group({
     email: ['iggnaxios@gmail.com', [Validators.required, Validators.email]],
-    password: ['1234567', [Validators.required, Validators.minLength(6)]]
+    password: ['123456', [Validators.required, Validators.minLength(6)]]
   });
 
   login() {
     const { email, password } = this.myForm.value;
     this.authService.login(email, password)
       .subscribe({
-        next: () => console.log('Everything Good'),
+        next: () => this.router.navigateByUrl('/dashboard'),
         error: (message) => {
           Swal.fire('Error', message, 'error')
         }
